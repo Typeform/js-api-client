0a52ac03d5414a4312c4be9da6199bfbe0fb702c
Michael Solati <mkslt04@gmail.com>
2019-07-23 14:10:33 -0700

    refactor(themes): abstract `create` and `update` methods to one function

--- src/themes.js
+++ src/themes.js
@@ -7,25 +7,8 @@ class Themes {
     this._http = _http
   }
 
-  create ({ background, colors, font, hasTransparentButton, name } = {}) {
-    // check if required properties are defined
-    if ([name, font, colors].includes(undefined)) {
-      throw new Error(`Please add the required fields`)
-    }
-
-    if (!FONTS_AVAILABLE.includes(font)) {
-      throw new Error(`${font} is not a valid font`)
-    }
-
-    return this._http.request({
-      method: 'post',
-      url: `/themes`,
-      background,
-      colors,
-      font,
-      has_transparent_button: hasTransparentButton,
-      name
-    })
+  create (args) {
+    return createOrUpdateTheme(this._http, args)
   }
 
   delete ({ id } = {}) {
@@ -53,27 +36,31 @@ class Themes {
     })
   }
 
-  update ({ id, background, colors, font, hasTransparentButton, name } = {}) {
-    // check if required properties are defined
-    const requiredProperties = { name, font, colors }
-    Object.getOwnPropertyNames(requiredProperties).forEach((property) => {
-      if (!requiredProperties[property]) {
-        throw new Error(`The property, ${property}, is required`)
-      }
-    })
-
-    if (!FONTS_AVAILABLE.includes(font)) {
-      throw new Error(`${font} is not a valid font`)
-    }
-
-    return this._http.request({
-      method: 'put',
-      url: `/themes/${id}`,
-      background,
-      colors,
-      font,
-      has_transparent_button: hasTransparentButton,
-      name
-    })
+  update (args) {
+    return createOrUpdateTheme(this._http, args)
   }
+}
+
+const createOrUpdateTheme = (http, { id, background, colors, font, hasTransparentButton, name } = {}) => {
+  // check if required properties are defined
+  const requiredProperties = { name, font, colors }
+  Object.getOwnPropertyNames(requiredProperties).forEach((property) => {
+    if (!requiredProperties[property]) {
+      throw new Error(`The property, ${property}, is required`)
+    }
+  })
+
+  if (!FONTS_AVAILABLE.includes(font)) {
+    throw new Error(`${font} is not a valid font`)
+  }
+
+  return http.request({
+    method: id ? 'put' : 'post',
+    url: id ? `/themes/${id}` : '/themes',
+    background,
+    colors,
+    font,
+    has_transparent_button: !!hasTransparentButton,
+    name
+  })
 }
