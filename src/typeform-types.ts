2386af3cdbf77cf1af7d8d8fb3dd6acb0f4e56a7
Nic Wilson <nicwilson@gmail.com>
2019-11-08 22:09:08 -0600

    fix(types): fix Condition.op type
    The operator `begins_with` should be `starts_with` per the docs
    https://developer.typeform.com/create/logic-jumps/

--- src/typeform-types.ts
+++ src/typeform-types.ts
@@ -166,7 +166,7 @@ export namespace Typeform {
     /**
      * Operator for the condition.
      */
-    op?: 'begins_with' | 'ends_with' | 'contains' | 'not_contains' | 'lower_than' | 'lower_equal_than' | 'greater_than'
+    op?: 'starts_with' | 'ends_with' | 'contains' | 'not_contains' | 'lower_than' | 'lower_equal_than' | 'greater_than'
     | 'greater_equal_than' | 'is' | 'is_not' | 'equal' | 'not_equal' | 'always' | 'on' | 'not_on' | 'earlier_than' | 'earlier_than_or_on'
     | 'later_than' | 'later_than_or_on'
     /**
