983f6a39270bf96bd90e3609442db3cf3aa7979c
vrybas <vladimir.rybas@typeform.com>
2024-07-23 08:40:36 +0200

    [TU-13737] reset modified files

--- src/webhooks.ts
+++ src/webhooks.ts
@@ -1,100 +1,99 @@
-import {Typeform} from './typeform-types'
+import { Typeform } from './typeform-types'
 
 export class Webhooks {
-    constructor(private _http: Typeform.HTTPClient) {
-    }
+  constructor(private _http: Typeform.HTTPClient) {}
 
-    public create(args: {
-        uid: string
-        tag: string
-        url: string
-        enabled?: boolean
-        secret?: string
-        verifySSL?: boolean
-    }): Promise<Typeform.Webhook> {
-        return createOrUpdateWebhook(this._http, args)
-    }
+  public create(args: {
+    uid: string
+    tag: string
+    url: string
+    enabled?: boolean
+    secret?: string
+    verifySSL?: boolean
+  }): Promise<Typeform.Webhook> {
+    return createOrUpdateWebhook(this._http, args)
+  }
 
-    public delete(args: { uid: string; tag: string }): Promise<null> {
-        const {uid, tag} = args
+  public delete(args: { uid: string; tag: string }): Promise<null> {
+    const { uid, tag } = args
 
-        return this._http.request({
-            method: 'delete',
-            url: `/forms/${uid}/webhooks/${tag}`,
-        })
-    }
+    return this._http.request({
+      method: 'delete',
+      url: `/forms/${uid}/webhooks/${tag}`,
+    })
+  }
 
-    public get(args: { uid: string; tag: string }): Promise<Typeform.Webhook> {
-        const {uid, tag} = args
+  public get(args: { uid: string; tag: string }): Promise<Typeform.Webhook> {
+    const { uid, tag } = args
 
-        return this._http.request({
-            method: 'get',
-            url: `/forms/${uid}/webhooks/${tag}`,
-        })
-    }
+    return this._http.request({
+      method: 'get',
+      url: `/forms/${uid}/webhooks/${tag}`,
+    })
+  }
 
-    public list(args: { uid: string }): Promise<Typeform.API.Webhooks.List> {
-        const {uid} = args
+  public list(args: { uid: string }): Promise<Typeform.API.Webhooks.List> {
+    const { uid } = args
 
-        return this._http.request({
-            method: 'get',
-            url: `/forms/${uid}/webhooks`,
-        })
-    }
+    return this._http.request({
+      method: 'get',
+      url: `/forms/${uid}/webhooks`,
+    })
+  }
+  
+  public toggle (args: { uid: string, tag: string, enabled: boolean }): Promise<Typeform.API.Webhooks.List> {
+    const { uid, tag, enabled } = args
 
-    public toggle(args: { uid: string, tag: string, enabled: boolean }): Promise<Typeform.API.Webhooks.List> {
-        const {uid, tag, enabled} = args
+    return this._http.request({
+      method: 'put',
+      url: `/forms/${uid}/webhooks/${tag}`,
+      data: {
+        enabled
+      }
+    })
+  }
 
-        return this._http.request({
-            method: 'put',
-            url: `/forms/${uid}/webhooks/${tag}`,
-            data: {
-                enabled
-            }
-        })
-    }
-
-    update(args: {
-        uid: string
-        tag: string
-        url: string
-        enabled?: boolean
-        secret?: string
-        verifySSL?: boolean
-    }): Promise<Typeform.Webhook> {
-        return createOrUpdateWebhook(this._http, args)
-    }
+  update(args: {
+    uid: string
+    tag: string
+    url: string
+    enabled?: boolean
+    secret?: string
+    verifySSL?: boolean
+  }): Promise<Typeform.Webhook> {
+    return createOrUpdateWebhook(this._http, args)
+  }
 }
 
 const createOrUpdateWebhook = (
-    http: Typeform.HTTPClient,
-    args: {
-        uid: string
-        tag: string
-        url: string
-        enabled?: boolean
-        secret?: string
-        verifySSL?: boolean
-    }
+  http: Typeform.HTTPClient,
+  args: {
+    uid: string
+    tag: string
+    url: string
+    enabled?: boolean
+    secret?: string
+    verifySSL?: boolean
+  }
 ): Promise<Typeform.Webhook> => {
-    const {uid, tag, url, enabled = false, secret, verifySSL} = args
+  const { uid, tag, url, enabled = false, secret, verifySSL } = args
 
-    if (!url) {
-        throw new Error(`Please provide an url for ${tag}`)
-    }
+  if (!url) {
+    throw new Error(`Please provide an url for ${tag}`)
+  }
 
-    if (!tag) {
-        throw new Error(`Please provide a tag name for the webhook`)
-    }
+  if (!tag) {
+    throw new Error(`Please provide a tag name for the webhook`)
+  }
 
-    return http.request({
-        method: 'put',
-        url: `/forms/${uid}/webhooks/${tag}`,
-        data: {
-            url,
-            enabled,
-            secret,
-            verify_ssl: verifySSL ? true : undefined,
-        },
-    })
+  return http.request({
+    method: 'put',
+    url: `/forms/${uid}/webhooks/${tag}`,
+    data: {
+      url,
+      enabled,
+      secret,
+      verify_ssl: verifySSL ? true : undefined,
+    },
+  })
 }
