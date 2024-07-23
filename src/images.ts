fa0a37de22d343fd4e9966ad1b0b5d46b37c0c68
vrybas <vladimir.rybas@typeform.com>
2024-07-23 08:07:25 +0200

    [TU-13737] indent images.ts

--- src/images.ts
+++ src/images.ts
@@ -1,88 +1,89 @@
-import { Typeform } from './typeform-types'
+import {Typeform} from './typeform-types'
 
 export class Images {
-  constructor(private _http: Typeform.HTTPClient) {}
-
-  public add(args: {
-    image?: string
-    url?: string
-    fileName: string
-  }): Promise<Typeform.Image> {
-    const { image, url, fileName } = args
-
-    return this._http.request({
-      method: 'post',
-      url: `/images`,
-      data: {
-        image,
-        file_name: fileName,
-        url,
-      },
-    })
-  }
-
-  public delete(args: { id: string }): Promise<null> {
-    const { id } = args
-
-    return this._http.request({
-      method: 'delete',
-      url: `/images/${id}`,
-    })
-  }
-
-  public get(args: {
-    id: string
-    size?: string
-    backgroundSize?: string
-    choiceSize?: string
-  }): Promise<Typeform.Image> {
-    const { id, size, backgroundSize, choiceSize } = args
-    const requestQuery: Typeform.Request = {
-      method: 'get',
-      url: `/images/${id}`,
-      headers: {
-        Accept: 'application/json',
-      },
+    constructor(private _http: Typeform.HTTPClient) {
     }
 
-    const choiceImageSizes = [
-      'default',
-      'thumbnail',
-      'supersize',
-      'supermobile',
-      'supersizefit',
-      'supermobilefit',
-    ]
+    public add(args: {
+        image?: string
+        url?: string
+        fileName: string
+    }): Promise<Typeform.Image> {
+        const {image, url, fileName} = args
 
-    if (size) {
-      if (['default', 'thumbnail', 'mobile'].includes(size)) {
-        requestQuery.url += `/image/${size}`
-      } else {
-        throw new Error(`Image size doesn't exist`)
-      }
-    } else if (backgroundSize) {
-      if (
-        ['default', 'thumbnail', 'mobile', 'tablet'].includes(backgroundSize)
-      ) {
-        requestQuery.url += `/background/${backgroundSize}`
-      } else {
-        throw new Error(`Image background size doesn't exist`)
-      }
-    } else if (choiceSize) {
-      if (choiceImageSizes.includes(choiceSize)) {
-        requestQuery.url += `/choice/${choiceSize}`
-      } else {
-        throw new Error(`Image choice size doesn't exist`)
-      }
+        return this._http.request({
+            method: 'post',
+            url: `/images`,
+            data: {
+                image,
+                file_name: fileName,
+                url,
+            },
+        })
     }
 
-    return this._http.request(requestQuery)
-  }
+    public delete(args: { id: string }): Promise<null> {
+        const {id} = args
 
-  public list(): Promise<Typeform.Image[]> {
-    return this._http.request({
-      method: 'get',
-      url: '/images',
-    })
-  }
+        return this._http.request({
+            method: 'delete',
+            url: `/images/${id}`,
+        })
+    }
+
+    public get(args: {
+        id: string
+        size?: string
+        backgroundSize?: string
+        choiceSize?: string
+    }): Promise<Typeform.Image> {
+        const {id, size, backgroundSize, choiceSize} = args
+        const requestQuery: Typeform.Request = {
+            method: 'get',
+            url: `/images/${id}`,
+            headers: {
+                Accept: 'application/json',
+            },
+        }
+
+        const choiceImageSizes = [
+            'default',
+            'thumbnail',
+            'supersize',
+            'supermobile',
+            'supersizefit',
+            'supermobilefit',
+        ]
+
+        if (size) {
+            if (['default', 'thumbnail', 'mobile'].includes(size)) {
+                requestQuery.url += `/image/${size}`
+            } else {
+                throw new Error(`Image size doesn't exist`)
+            }
+        } else if (backgroundSize) {
+            if (
+                ['default', 'thumbnail', 'mobile', 'tablet'].includes(backgroundSize)
+            ) {
+                requestQuery.url += `/background/${backgroundSize}`
+            } else {
+                throw new Error(`Image background size doesn't exist`)
+            }
+        } else if (choiceSize) {
+            if (choiceImageSizes.includes(choiceSize)) {
+                requestQuery.url += `/choice/${choiceSize}`
+            } else {
+                throw new Error(`Image choice size doesn't exist`)
+            }
+        }
+
+        return this._http.request(requestQuery)
+    }
+
+    public list(): Promise<Typeform.Image[]> {
+        return this._http.request({
+            method: 'get',
+            url: '/images',
+        })
+    }
 }
